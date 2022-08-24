401b71e68343f7606a38d234c02f159044fd6e08
Lucie Chevalier <chevalierlucie81@gmail.com>
2022-08-24 14:06:41 +0200

    Added changes to Reservation controller, model and view

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,22 +5,29 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    # Initialisation du costume et de la reservation
     @costume = Costume.find(params[:costume_id])
     @reservation = Reservation.new(reservation_params)
+
+    # Mise à jour du current_user et des colonnes manquantes de la réservation
+    current_user.update(address: reservation_params[:address]) if current_user.address.nil?
     @reservation.costume = @costume
-    @reservation.save
-    redirect_to costume_reservations(@costume)
+    @reservation.total_amount = ((@reservation.end_date - @reservation.start_date + 1) * @costume.price_per_day).to_i
+    @reservation.renter = current_user
+    @reservation.status = "Pending"
+    if @reservation.save
+      redirect_to costume_reservations_path(@costume)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    # Redirection vers la page des réservations du renter
+    #redirect_to costume_reservations_path(@costume)
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:comment, :start_date, :end_date)
+    params.require(:reservation).permit(:comment, :start_date, :end_date, :address)
   end
 end
-
-# t.text :comment
-# t.string :status
-# t.date :start_date
-# t.date :end_date
-# t.integer :total_amount
